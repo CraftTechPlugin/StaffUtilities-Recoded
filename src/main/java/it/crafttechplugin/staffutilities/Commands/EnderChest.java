d099d5ff361a190fc8d01ffeba0bb31c7db78380
GhostKnight001 <luca4316812@gmail.com>
2023-07-14 18:20:51 +0200

    added Ec command added aliases for EnderChest and Vanish added locales

---
+++ src/main/java/it/crafttechplugin/staffutilities/Commands/EnderChest.java
@@ -0,0 +1,49 @@
+package it.crafttechplugin.staffutilities.Commands;
+
+import it.crafttechplugin.staffutilities.Main;
+import it.crafttechplugin.staffutilities.Utils.ColorTranslateUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+
+public class EnderChest implements CommandExecutor {
+    YamlConfiguration msg = (YamlConfiguration) Main.data;
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args) {
+
+        if(!(sender instanceof Player)) {
+            sender.sendMessage(ColorTranslateUtil.getColor(msg.getString("Messages.Prefix") + msg.getString("Messages.noPerms")));
+            return false;
+        }
+        else {
+            Player p = (Player) sender;
+            if (p.hasPermission("staffutilities.enderchest.self")) {
+                if (args.length == 0) {
+                    p.openInventory(p.getEnderChest());
+                }
+                if (args.length >= 3){
+                    String pc = p.getName();
+                    if (args[0].equalsIgnoreCase(pc)){
+                        p.openInventory(p.getEnderChest());
+                    }
+                }
+            } else if (p.hasPermission("staffutilities.enderchest.other")) {
+                if (args.length == 0) {
+                    sender.sendMessage(ColorTranslateUtil.getColor(msg.getString("Messages.Prefix")) + msg.getString("Messages.noArgs"));
+                } else if (args.length >= 3) {
+                    Player t = Bukkit.getPlayer(args[0]);
+                    p.openInventory(t.getEnderChest());
+                }
+            }
+        }
+
+        return false;
+    }
+}
--- src/main/java/it/crafttechplugin/staffutilities/Main.java
+++ src/main/java/it/crafttechplugin/staffutilities/Main.java
@@ -6,43 +6,77 @@ import it.crafttechplugin.staffutilities.Commands.Teleport.tphere;
 import it.crafttechplugin.staffutilities.Events.Events;
 import it.crafttechplugin.staffutilities.UpdateCheck.UpdateChecker;
 import it.crafttechplugin.staffutilities.Utils.ColorTranslateUtil;
-import it.crafttechplugin.staffutilities.Commands.*;
-import it.crafttechplugin.staffutilities.Commands.Teleport.*;
-import it.crafttechplugin.staffutilities.Events.*;
-import it.crafttechplugin.staffutilities.UpdateCheck.*;
-import it.crafttechplugin.staffutilities.Utils.*;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
 public final class Main extends JavaPlugin implements Listener {
+
     public static Main plugin;
 
+    public static FileConfiguration data = YamlConfiguration.loadConfiguration(new File(Main.getInstance().getDataFolder(), "locales.En-en.yml"));
+
+    Double configVersion = Main.getInstance().getConfig().getDouble("version");
+
     public void onEnable() {
-        new UpdateChecker(this, 108874).getLastestVersion(version ->{
-            if(this.getDescription().getVersion().equalsIgnoreCase(version)){
-                System.out.println(ColorTranslateUtil.getColor("StaffUtilities » An update was found!"));
-            }else{
-                System.out.println(ColorTranslateUtil.getColor("StaffUtilities » An update was found!"));
+        boolean cup = Main.getInstance().getConfig().getBoolean("check-update");
+        Double cVersion = 1.0;
+
+        if (cup) {
+            if (cVersion.equals(configVersion)){
+
+                new UpdateChecker(this, 108874).getLastestVersion(version ->{
+
+                    if(this.getDescription().getVersion().equalsIgnoreCase(version)){
+
+                        System.out.println(ColorTranslateUtil.getColor("StaffUtilities » An update was found!"));
+
+                    }else{
+
+                        System.out.println(ColorTranslateUtil.getColor("StaffUtilities » An update was found!"));
+                    }
+                });
             }
-        });
+
+        }
 
         plugin = this;
 
+        getCommand("invsee").setExecutor(new InvSee());
+
+        getCommand("enderchest").setExecutor(new EnderChest());
+
         getCommand("gmc").setExecutor(new Gmc());
+
         getCommand("fly").setExecutor(new Fly());
+
         getCommand("tp").setExecutor(new tp());
+
         getCommand("tphere").setExecutor(new tphere());
+
         getCommand("pl").setExecutor(new PLHider());
+
         getCommand("staffutilities").setExecutor(new StaffUtilitiesCommand());
+
         getCommand("plugins").setExecutor(new PLHider());
+
         getCommand("startevent").setExecutor(new StartEvent());
+
         getCommand("vanish").setExecutor(new Vanish());
+
         getServer().getPluginManager().registerEvents(new Events(), this);
+
         getServer().getPluginManager().registerEvents(this, this);
 
+
         getConfig().options().copyDefaults(true);
+
         saveConfig();
     }
 
@@ -53,14 +87,23 @@ public final class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
+
         Player p = e.getPlayer();
+
         if(p.hasPermission("staffutilities.updatecheck") || p.hasPermission("staffutilities.*")) {
+
             new UpdateChecker(this, 108874).getLastestVersion(version -> {
+
                 if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+
                     p.sendMessage(ColorTranslateUtil.getColor("&b&lSTAFFUTILITIES &8» &bAn update was found!"));
+
                     p.sendMessage(ColorTranslateUtil.getColor("&7(https://www.spigotmc.org/resources/108874/)"));
+
                 } else {
+
                     p.sendMessage(ColorTranslateUtil.getColor("&b&lSTAFFUTILITIES &8» &bAn update was found!"));
+
                     p.sendMessage(ColorTranslateUtil.getColor("&7(https://www.spigotmc.org/resources/108874/)"));
                 }
             });
