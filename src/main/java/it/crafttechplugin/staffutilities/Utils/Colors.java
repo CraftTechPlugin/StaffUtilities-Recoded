0278fb8df42790529018d271eef55563e97c46e6
GhostKnight001 <luca4316812@gmail.com>
2023-07-15 18:29:21 +0200

    added Moderation

--- src/main/java/it/crafttechplugin/staffutilities/Utils/Colors.java
+++ src/main/java/it/crafttechplugin/staffutilities/Utils/Colors.java
@@ -2,7 +2,7 @@ package it.crafttechplugin.staffutilities.Utils;
 
 import org.bukkit.ChatColor;
 
-public class ColorTranslateUtil {
+public class Colors {
     public static String getColor(String s) {
         return ChatColor.translateAlternateColorCodes('&', s);
     }
